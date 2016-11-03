61bae1bd87512eb3870a1f6509133cc1b62d3688
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2016-11-03 03:49:29 -0200

    updates para apresentacao parcial

---
+++ VS_Jogo_SW/scripts/lista_portas.py
@@ -0,0 +1,17 @@
+'''
+    Programa que lista as portas "seriais" conectadas
+    ao computador e salva elas em um arquivo txt com nome
+    lista_seriais.txt, sendo que acada linha contém a string
+    com o nome da porta serial
+
+    02/11/2016
+    Gustavo Voltani von Atzingen
+'''
+
+import serial.tools.list_ports as verificador
+
+with open('lista_seriais.txt','w') as f:
+    dispositivos = verificador.comports()
+    for dispositivo in dispositivos:
+        f.write(dispositivo.device + '\n')
+f.close()
